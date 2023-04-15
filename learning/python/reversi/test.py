fe77da7190f0855647bbfa6613be346d5f3669bf
Alex J <asj18a@acu.edu>
2023-04-15 13:15:01 -0500

    Game functionality working

--- learning/python/reversi/test.py
+++ learning/python/reversi/test.py
@@ -1,45 +1,52 @@
 from bitarray import bitarray
+from curses import wrapper
 
 
 class Board:
-    def __init__(self, board="--------------XO----OX--------------", next="X"):
-        if type(board) == tuple and type(board[0]) == bitarray and type(board[1]) == bitarray:
-            self.board = board[0]
-            self.mask = board[1]
-            if type(next) == bool:
-                self.next = next
-            else:
-                if type(next) == str:
-                    self.next = 1 if next == "X" else 0
-                else:
-                    raise TypeError(f"Expected next to be bool, since board is a tuple, but got type {type(next)}")
-        elif type(board) == str:
-            self.board = bitarray(board.replace(
-                "-", "0").replace("X", "1").replace("O", "0"))
-            self.mask = bitarray(board.replace(
-                "-", "0").replace("X", "1").replace("O", "1"))
-            if type(next) == str:
-                self.next = 1 if next == "X" else 0
-            else:
-                if type(next) == bool:
-                    self.next = next
-                else:
-                    raise TypeError(f"Expected next to be str, since board is a str, but got type {type(next)}")
+    board: bitarray
+    mask: bitarray
+    next: bool
+
+    def __init__(self, board: bitarray, mask: bitarray, next: bool = True):
+        self.board = board
+        self.mask = mask
+        self.next = next
+
+    @staticmethod
+    def from_string(board: str, next: str):
+        b = bitarray(board
+                     .replace("X", "1")
+                     .replace("O", "0")
+                     .replace("-", "0"))
+        m = bitarray(board
+                     .replace("X", "1")
+                     .replace("O", "1")
+                     .replace("-", "0"))
+
+        return Board(b, m, next == "X")
 
     def check_directions(self, x: int) -> int:
-        directions = bitarray(8)
+        directions = bitarray('0' * 8)
 
-        # Check clockwise: top left, top, top right, right, bottom right, bottom, bottom left, left
+        # Check clockwise:
+        # top left, top, top right,
+        # right, bottom right,
+        # bottom, bottom left, left
         for n, offset in enumerate([-7, -6, -5, 1, 7, 6, 5, -1]):
             to_flip = False
+            old = x
             for i in range(x + offset, -1 if offset < 0 else 36, offset):
-                if not (self.board[i] or self.mask[i]):  # If the space is empty
-                    break
-                if not ((self.board[i] & self.mask[i]) ^ self.next):
-                    to_flip = True
-                elif to_flip:
-                    directions[n] = 1
-                    break
+                if -1 <= (old % 6) - (i % 6) <= 1:  # bounds check
+                    if not self.mask[i]:  # If the space is empty
+                        break
+                    if (self.board[i] & self.mask[i]) ^ self.next:
+                        to_flip = True
+                    elif to_flip:
+                        directions[n] = 1
+                        break
+                    else:
+                        break
+                    old = i
                 else:
                     break
         return directions
@@ -54,7 +61,12 @@ class Board:
 
         b = "".join(b)
 
-        return f"{b[:6]}\n{b[6:12]}\n{b[12:18]}\n{b[18:24]}\n{b[24:30]}\n{b[30:]}"
+        return f"{b[:6]}\n" \
+            f"{b[6:12]}\n" \
+            f"{b[12:18]}\n" \
+            f"{b[18:24]}\n" \
+            f"{b[24:30]}\n" \
+            f"{b[30:]}"
 
     def move(self, x: int, directions: int):
         new_mask = bitarray(self.mask)
@@ -66,15 +78,13 @@ class Board:
         for n, offset in enumerate([-7, -6, -5, 1, 7, 6, 5, -1]):
             if directions[n]:
                 for i in range(x + offset, -1 if offset < 0 else 36, offset):
-                    if not ((self.board[i] & self.mask[i]) ^ self.next):
+                    if (self.board[i] & self.mask[i]) ^ self.next:
                         new_board[i] = self.next
+                        new_mask[i] = 1
                     else:
                         break
 
-        print(x)
-        print(new_board)
-        print(new_mask)
-        return Board((new_board, self.mask), not self.next)
+        return Board(new_board, new_mask, not self.next)
 
     def moves(self):
         moves = []
@@ -82,21 +92,46 @@ class Board:
         for i in range(36):
             if not self.mask[i]:
                 valid = self.check_directions(i)
-                if valid:
+                if valid.any():
                     moves.append(self.move(i, valid))
 
         return moves
 
+    def pretty(self, color: bool = False) -> str:
+        if color:
+            # Print the background in gray
+            # Make the X's Black and the O's White
+            def color(s: str):
+                return f"\033[48;5;8m\033[38;5;0m{s}\033[0m"
+        else:
+            def color(s: str):
+                return s
 
-def main():
-    # board = Board("--------------XO----OX--------------", "X")
-    board = Board("OXXXXX-XXXXX--XO----OX--------------", "O")
-    print(f"Next: {board.next}\n{board}\n")
+        b = "╔═══╦═══╦═══╦═══╦═══╦═══╗\n║"
+        for i in range(36):
+            if i % 6 == 0 and i != 0:
+                b += "\n╠═══╬═══╬═══╬═══╬═══╬═══╣\n║"
 
-    for move in board.moves():
-        pass
-        # print(f"Next: {move.next}\n{move}\n")
+            if self.mask[i]:
+                b += f" {'X' if self.board[i] else 'O'} ║"
+            else:
+                b += "   ║"
+        b += "\n╚═══╩═══╩═══╩═══╩═══╩═══╝"
+        return color(b)
+
+
+def main(stdscr):
+    stdscr.clear()
+
+    _ = Board(
+        bitarray('000000000000001000000100000000000000'),
+        bitarray('000000000000001100001100000000000000'),
+        True
+    )
+    board = Board.from_string("--------------XO----OX--------------", "X")
+
+    stdscr.addstr(board.pretty())
 
 
 if __name__ == "__main__":
-    main()
+    wrapper(main)
