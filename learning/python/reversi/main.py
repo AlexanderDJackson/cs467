2ec94b5fc79f96b8daeb557d3647004f1f35d3b8
Alex J <asj18a@acu.edu>
2023-04-15 14:53:10 -0500

    Testing

--- learning/python/reversi/main.py
+++ learning/python/reversi/main.py
@@ -1,92 +1,137 @@
+from bitarray import bitarray
+from curses import wrapper
+
+
 class Board:
-    def __init__(self, board = "--------------XO----OX--------------", next = "X"):
+    board: bitarray
+    mask: bitarray
+    next: bool
+
+    def __init__(self, board: bitarray, mask: bitarray, next: bool = True):
         self.board = board
+        self.mask = mask
         self.next = next
 
-    def check_directions(self, x: int) -> int:
-        directions = 0
+    @staticmethod
+    def from_string(board: str, next: str):
+        b = bitarray(board
+                     .replace("X", "1")
+                     .replace("O", "0")
+                     .replace("-", "0"))
+        m = bitarray(board
+                     .replace("X", "1")
+                     .replace("O", "1")
+                     .replace("-", "0"))
 
-        # Check clockwise: top left, top, top right, right, bottom right, bottom, bottom left, left
-        for offset in (-7, -6, -5, 1, 7, 6, 5, -1):
+        return Board(b, m, next == "X")
+
+    def check_directions(self, x: int) -> int:
+        directions = bitarray('0' * 8)
+
+        # Check clockwise:
+        # top left, top, top right,
+        # right, bottom right,
+        # bottom, bottom left, left
+        for n, offset in enumerate([-7, -6, -5, 1, 7, 6, 5, -1]):
             to_flip = False
-            for i in range(x + offset, -1 if offset < 0 else 36, offset): 
-                if self.board[i] == "-":
-                    break
-                elif self.board[i] != self.next:
-                    to_flip = True
-                elif to_flip:
-                    directions |= 1
-                    break
+            old = x
+            for i in range(x + offset, -1 if offset < 0 else 36, offset):
+                if -1 <= (old % 6) - (i % 6) <= 1:  # bounds check
+                    if not self.mask[i]:  # If the space is empty
+                        break
+                    if (self.board[i] & self.mask[i]) ^ self.next:
+                        to_flip = True
+                    elif to_flip:
+                        directions[n] = 1
+                        break
+                    else:
+                        break
+                    old = i
                 else:
                     break
-            directions <<= 1
-            print(f"Directions: {bin(directions)}")
-        return directions >> 1
+        return directions
 
     def __str__(self) -> str:
-        b = self.board
-        return f"{b[:6]}\n{b[6:12]}\n{b[12:18]}\n{b[18:24]}\n{b[24:30]}\n{b[30:]}"
+        b = []
+        for i in self.mask:
+            b.append("X" if i else "-")
+
+        for i in range(36):
+            b[i] = "O" if self.board[i] ^ self.mask[i] else b[i]
+
+        b = "".join(b)
+
+        return f"{b[:6]}\n" \
+            f"{b[6:12]}\n" \
+            f"{b[12:18]}\n" \
+            f"{b[18:24]}\n" \
+            f"{b[24:30]}\n" \
+            f"{b[30:]}"
 
     def move(self, x: int, directions: int):
-        new_board = list(self.board)
+        new_mask = bitarray(self.mask)
+        new_mask[x] = 1
+
+        new_board = bitarray(self.board)
         new_board[x] = self.next
 
-        if directions == 0:
-            self
-
-        print(f"Directions: {bin(directions)}")
-        for offset in (-7, -6, -5, 1, 7, 6, 5, -1):
-            if directions & 0b10000000:
-                for i in range(x + offset, -1 if offset < 0 else 36, offset): 
-                    if self.board[i] == self.next:
-                        break
-                    elif self.board[i] != "-":
-                        print(f"Writing {self.next} to {i}")
+        for n, offset in enumerate([-7, -6, -5, 1, 7, 6, 5, -1]):
+            if directions[n]:
+                for i in range(x + offset, -1 if offset < 0 else 36, offset):
+                    if (self.board[i] & self.mask[i]) ^ self.next:
                         new_board[i] = self.next
-            directions <<= 1
+                        new_mask[i] = 1
+                    else:
+                        break
 
-        return Board("".join(new_board), "X" if self.next == "O" else "O")
+        return Board(new_board, new_mask, not self.next)
 
-    """
-    Reversi Board Format:
-        --------------XO----OX--------------
-
-    Legal Moves (marked with a "*"):
-        ------
-        ---*--
-        --XO*-
-        -*OX--
-        --*---
-        ------
-    """
     def moves(self):
         moves = []
 
         for i in range(36):
-            valid = self.check_directions(i)
-            if self.board[i] == "-" and valid != 0:
-                moves.append(self.move(i, valid))
+            if not self.mask[i]:
+                valid = self.check_directions(i)
+                if valid.any():
+                    moves.append(self.move(i, valid))
 
         return moves
 
+    def pretty(self, color: bool = False) -> str:
+        if color:
+            # Print the background in gray
+            # Make the X's Black and the O's White
+            def color(s: str):
+                return f"\033[48;5;8m\033[38;5;0m{s}\033[0m"
+        else:
+            def color(s: str):
+                return s
 
-def count(board: Board) -> int:
-    m = board.moves()
+        b = "╔═══╦═══╦═══╦═══╦═══╦═══╗\n║"
+        for i in range(36):
+            if i % 6 == 0 and i != 0:
+                b += "\n╠═══╬═══╬═══╬═══╬═══╬═══╣\n║"
 
-    if len(m) == 0:
-        return 1
-    else:
-        return sum(count(move) for move in m)
+            if self.mask[i]:
+                b += f" {'X' if self.board[i] else 'O'} ║"
+            else:
+                b += "   ║"
+        b += "\n╚═══╩═══╩═══╩═══╩═══╩═══╝"
+        return color(b)
 
-def main():
-    # board = Board("--------------XO----OX--------------", "X")
-    board = Board("OXXXXX-XXXXX--XO----OX--------------", "O")
-    print(f"Next: {board.next}\n{board}\n")
-    board = board.move(6, board.check_directions(6))
-    print(f"Next: {board.next}\n{board}\n")
 
-    # for move in moves(board):
-        # print(count(move))
+def main(stdscr):
+    stdscr.clear()
+
+    _ = Board(
+        bitarray('000000000000001000000100000000000000'),
+        bitarray('000000000000001100001100000000000000'),
+        True
+    )
+    board = Board.from_string("--------------XO----OX--------------", "X")
+
+    stdscr.addstr(board.pretty())
+
 
 if __name__ == "__main__":
-    main()
+    wrapper(main)
---
+++ learning/python/reversi/test_reversi.py
@@ -0,0 +1,254 @@
+from test import Game, pretty_print
+from randomplayer import RandomPlayer
+
+# decide if pieces are flippable in this direction
+
+
+def flips(board, index, piece, step):
+    other = ('X' if piece == 'O' else 'O')
+    # is an opponent's piece in first spot that way?
+    here = index + step
+    if board[here] != other:
+        return False
+    # diff : how index mod is changing each step
+    diff = index % 6 - here % 6
+    while here % 6 - (here + step) % 6 == diff and \
+            here > 0 and here < 36 and \
+            board[here] == other:
+        here = here + step
+
+    return here % 6 - (here + step) % 6 == diff and \
+        here > 0 and here < 36 and \
+        board[here] == piece
+
+# decide if given move (index x) is valid for player p
+
+
+def validMove(b, x, p):  # board, index, piece
+    # invalid index
+    if x < 0 or x >= 36:
+        return False
+    # space already occupied
+    if b[x] != '-':
+        return False
+    # otherwise, check for flipping pieces
+    up = x >= 12   # at least third row down
+    down = x < 24   # at least third row up
+    left = x % 6 > 1  # at least third column
+    right = x % 6 < 4  # not past fourth column
+    return (left and flips(b, x, p, -1)  # left
+            or up and left and flips(b, x, p, -7)  # up/left
+            or up and flips(b, x, p, -6)  # up
+            or up and right and flips(b, x, p, -5)  # up/right
+            or right and flips(b, x, p, 1)  # right
+            or down and right and flips(b, x, p, 7)  # down/right
+            or down and flips(b, x, p, 6)  # down
+            or down and left and flips(b, x, p, 5))  # down/left
+
+# actually flip pieces in this direction
+# assume validity has already been checked
+
+
+def applyFlip(board, index, piece, step):
+    other = ('X' if piece == 'O' else 'O')
+    # starting point
+    here = index + step
+    while board[here] == other:
+        board = board[:here] + piece + board[here+1:]
+        here = here + step
+    return board
+
+# actually flip pieces in this direction
+
+
+def applyMove(x, p):  # index, piece
+    global gameboard
+    b = gameboard
+
+    # if not valid move, stop here
+    if not validMove(b, x, p):
+        return False
+
+    up = x >= 12   # at least third row down
+    down = x < 24   # at least third row up
+    left = x % 6 > 1  # at least third column
+    right = x % 6 < 4  # not past fourth column
+
+    # flip everything that should be flipped
+    if left and flips(b, x, p, -1):  # left
+        b = applyFlip(b, x, p, -1)
+    if up and left and flips(b, x, p, -7):  # up/left
+        b = applyFlip(b, x, p, -7)
+    if up and flips(b, x, p, -6):  # up
+        b = applyFlip(b, x, p, -6)
+    if up and right and flips(b, x, p, -5):  # up/right
+        b = applyFlip(b, x, p, -5)
+    if right and flips(b, x, p, 1):  # right
+        b = applyFlip(b, x, p, 1)
+    if down and right and flips(b, x, p, 7):  # down/right
+        b = applyFlip(b, x, p, 7)
+    if down and flips(b, x, p, 6):  # down
+        b = applyFlip(b, x, p, 6)
+    if down and left and flips(b, x, p, 5):  # down/left
+        b = applyFlip(b, x, p, 5)
+    # and put a new piece here too
+    b = b[:x] + p + b[x+1:]
+    # save modified gameboard
+    gameboard = b
+
+
+def printBoard(board):
+    print()
+    print("##########")
+    print("# " + board[0: 6] + " #")
+    print("# " + board[6:12] + " #")
+    print("# " + board[12:18] + " #")
+    print("# " + board[18:24] + " #")
+    print("# " + board[24:30] + " #")
+    print("# " + board[30:36] + " #")
+    print("##########")
+    print()
+
+# how many moves does this player have currently available?
+
+
+def countPossibleMoves(board, piece):
+    movesLeft = 0
+    for i in range(36):
+        movesLeft = movesLeft + validMove(board, i, piece)
+    return movesLeft
+
+# game score given board layout
+# X wins if positive, O wins if negative, tie if zero
+
+
+def getEndgameStatus(board):
+
+    countX = 0
+    countO = 0
+    for i in range(36):
+        countX = countX + (board[i] == 'X')
+        countO = countO + (board[i] == 'O')
+
+    return countX - countO
+
+
+# global variables
+gameboard = "--------------XO----OX--------------"
+gameover = False
+
+playerX = RandomPlayer('X')
+playerO = RandomPlayer('O')
+# O = Agent('O') # use this when agent is implemented
+
+# counters for tracking wins over multiple trials
+numWinX = 0
+numWinO = 0
+numTied = 0
+
+moves = []
+
+
+def log(moves):
+    global gameboard
+    gameboard = "--------------XO----OX--------------"
+    with open('given.txt', 'w') as g:
+        with open('mine.txt', 'w') as m:
+            g.write(f"X's Turn:\n{pretty_print(gameboard)}\n")
+            m.write(f"X's Turn:\n{pretty_print(str(Game.from_string(gameboard, 'X')))}\n")
+            player = 'O'
+            for move in moves:
+                print(gameboard)
+                board = Game.from_string(gameboard, 'X')
+                applyMove(move, player)
+                g.write(f"{player} played {move}:\n{pretty_print(gameboard)}\n")
+                m.write(
+                    f"{player} played {move}:\n{pretty_print(str(board.move(move, board.check_directions(move))))}\n")
+                player = 'X' if player == 'O' else 'O'
+
+# how many games do you want to play?
+for g in range(2500):
+    # reset global variables for new game
+    gameboard = "--------------XO----OX--------------"
+    gameover = False
+
+    g = open('given.txt', 'w')
+    m = open('mine.txt', 'w')
+    
+    # play game until done
+    move = 1
+    while not gameover:
+        # assert countPossibleMoves(gameboard, 'O') == \
+        # len(Game.from_string(gameboard, 'O').moves())
+
+        if countPossibleMoves(gameboard, 'X') > 0:
+            play = -1
+            while not validMove(gameboard, play, 'X'):
+                play = playerX.getMove(gameboard)
+            temp = Game.from_string(gameboard, 'X')
+            applyMove(play, 'X')
+            directions = temp.check_directions(play)
+            g.write(f"O played {play}:\n{pretty_print(gameboard)}\n")
+            m.write(
+                f"O played {play}:\n{pretty_print(str(temp.move(play, directions)))}\n")
+            try:
+                assert gameboard == str(temp.move(play, directions))
+            except AssertionError:
+                g.close()
+                m.close()
+                exit()
+
+        # player O
+        if countPossibleMoves(gameboard, 'O') > 0:
+            play = -1
+            while not validMove(gameboard, play, 'O'):
+                play = playerO.getMove(gameboard)
+            temp = Game.from_string(gameboard, 'O')
+            applyMove(play, 'O')
+            directions = temp.check_directions(play)
+            g.write(f"O played {play}:\n{pretty_print(gameboard)}\n")
+            m.write(
+                f"O played {play}:\n{pretty_print(str(temp.move(play, directions)))}\n")
+            try:
+                assert gameboard == str(temp.move(play, directions))
+            except AssertionError:
+                g.close()
+                m.close()
+                exit()
+
+        # if game over
+        if countPossibleMoves(gameboard, 'X') + \
+                countPossibleMoves(gameboard, 'O') == 0:
+            status = getEndgameStatus(gameboard)
+            if status > 0:  # X wins
+                playerX.endGame(1, gameboard)
+                playerO.endGame(-1, gameboard)
+                numWinX = numWinX + 1
+                # print( "X wins by " + str(status) + " pieces" )
+            elif status < 0:  # O wins
+                playerX.endGame(-1, gameboard)
+                playerO.endGame(1, gameboard)
+                numWinO = numWinO + 1
+                # print( "O wins by " + str(-status) + " pieces" )
+            else:  # status == 0, tie game
+                playerX.endGame(0, gameboard)
+                playerO.endGame(0, gameboard)
+                numTied = numTied + 1
+                # print( "Tie game" )
+            gameover = True
+            # printBoard(gameboard)
+
+        move = move + 1
+
+    # when running thousands of learning trials,
+    #   periodic updates are nice confirmation
+    #   that everything's still running
+#   if (numWinX + numWinO + numTied) % 1000 == 0:
+#      print( "Completed " + str(numWinX + numWinO + numTied) )
+
+playerX.stopPlaying()
+playerO.stopPlaying()
+
+print("X   : " + str(numWinX) + " games")
+print("O   : " + str(numWinO) + " games ***")
+print("Tie : " + str(numTied) + " games")
